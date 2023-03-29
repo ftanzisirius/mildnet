7c82a8739bb6e389e8f48a057274c41756dd0879
federicotanzi <federicotanzi@sirius.com.ar>
2023-03-29 12:28:49 -0300

    Test

--- trainer/task.py
+++ trainer/task.py
@@ -76,15 +76,15 @@ def main(job_dir, data_path, model_id, weights_path, loss, train_csv, val_csv, b
     if optimizer == "mo":
         model.compile(loss=_loss_tensor,
                       optimizer=tf.compat.v1.train.MomentumOptimizer(learning_rate=lr, momentum=0.9, use_nesterov=True),
-                      metrics=[accuracy],
-                      run_eagerly=True
+                      metrics=[accuracy]
                       )
+        model.run_eagerly = True
     elif optimizer == "rms":
         model.compile(loss=_loss_tensor,
                       optimizer=tf.compat.v1.train.RMSPropOptimizer(lr),
-                      metrics=[accuracy],
-                      run_eagerly=True
+                      metrics=[accuracy]
                       )
+        model.run_eagerly = True
     else:
         logging.error("Optimizer not supported")
         return
